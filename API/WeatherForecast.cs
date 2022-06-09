356a84bd791d37c75d6a9d478fabcb94117fd10e
shanegracias <shanegracias7@gmail.com>
2022-06-09 11:46:56 +0530

    .net 6 project changes

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -1,12 +1,16 @@
-namespace API;
-
-public class WeatherForecast
+using System;
+namespace API
 {
-    public DateTime Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string Summary { get; set; }
+    }
+
 }
+
