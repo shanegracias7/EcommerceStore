852419874368e812e45be69e84f09a9e7dddae68
shanegracias <shanegracias7@gmail.com>
2022-06-11 09:27:25 +0530

    bug fix

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -22,13 +22,13 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await context.Products.ToListAsync();
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public  async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await context.Products.FindAsync(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
