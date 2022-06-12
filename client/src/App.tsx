b34be0b36bcccd8201a8c5fd8a9148669c43130c
shanegracias <shanegracias7@gmail.com>
2022-06-12 22:31:46 +0530

    setting up CORS configuration implementing useState hook+ useEffect hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [products,setProducts] = useState([
+    {name:'Product1',price:'100'},
+    {name:'product2',price:'100'}
+  ]);
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/products")
+      .then(response=>response.json())
+      .then(data=>setProducts(data))
+  },[]
+ )
+
+  function addProduct(){
+    setProducts(prevState=>[...prevState,
+      {name:'product'+prevState.length,price:'100'}
+    ])
+  }
   return (
     <div>
-      <h1>STORE</h1> 
+      <ul>
+        {products.map((item,index)=>(
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>add product</button>
     </div>
   );
 }
