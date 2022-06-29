c6a9e730958e9f612a4de6b50a9e1fef2612ad34
shanegracias <shanegracias7@gmail.com>
2022-06-29 09:26:55 +0530

    create basket controller

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseAPIController
+    {
+        private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+                .Include(i=>i.Items)
+                .ThenInclude(p=>p.Product)
+                .FirstOrDefaultAsync(x=>x.BuyerId==Request.Cookies["buyerId"]);
+
+            if(basket == null) return NotFound();
+            return basket;
+            
+        }
+        
+    }
+}
