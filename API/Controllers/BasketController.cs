38c1bcd547c52645f7d92bb460eaadae184691a7
shanegracias <shanegracias7@gmail.com>
2022-06-30 14:50:59 +0530

    shaping data to return in basket controller

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,12 +21,27 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasket()
+        public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
 
             if (basket == null) return NotFound();
-            return basket;
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                                                    {
+                                                        ProductId = item.ProductId,
+                                                        Name = item.Product.Name,
+                                                        Description = item.Product.Description,
+                                                        Price = item.Product.Price,
+                                                        PictureURL = item.Product.PictureURL,
+                                                        Type = item.Product.Type,
+                                                        Brand = item.Product.Brand,
+                                                        Quantity = item.Product.Quantity
+                                                    }).ToList()
+            };
 
         }
 
