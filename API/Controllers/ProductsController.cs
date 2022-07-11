a680c8980aaa611b34e80c1344eb198ed2d80323
shanegracias <shanegracias7@gmail.com>
2022-07-11 11:18:07 +0530

    adding method to grt filters in product controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -21,7 +21,7 @@ namespace API.Controllers
    
         }
         [HttpGet]
-        public async Task<ActionResult<PagedList<Product>>> GetProducts(ProductParams productParams)
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
             var query =  _context.Products
                 .Sort(productParams.OrderBy)
@@ -30,8 +30,8 @@ namespace API.Controllers
                 .AsQueryable();
 
             var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
-            Response.Headers.Add("Pagination",JsonSerializer.Serialize(products.MetaData));
 
+            Response.AddPaginationHeader(products.MetaData);
             return products;
         }
 
@@ -41,6 +41,15 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
             if(product==null) return NotFound();
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p=>p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p=>p.Type).Distinct().ToListAsync();
+
+            return Ok( new {brands,types});
         }
     }
 }
---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.RequestHelpers;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, MetaData metaData)
+        {
+            var option = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+            response.Headers.Add("Pagination",JsonSerializer.Serialize(metaData,option));
+            response.Headers.Add("Access-Control-Expose-Header","Pagination");
+        }
+    }
+}
